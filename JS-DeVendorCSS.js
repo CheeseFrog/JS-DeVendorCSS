02af74c70b8849fd79cd08ea3717d86ecee747bc
CheeseFrog <au.yousef+dev@gmail.com>
2014-02-15 15:53:42 +1100

    Create JS-DeVendorCSS.js
    Usage: DeVendorCSS('css_property') --> ['vendor_prefix', 'js_property_name', 'css_property_name']

---
+++ JS-DeVendorCSS.js
@@ -0,0 +1,17 @@
+
+// Usage:
+//  DeVendorCSS('css_property') --> returns an array: ['vendor_prefix', 'js_property_name', 'css_property_name']
+
+// Example #1:
+//  DeVendorCSS('transform') --> returns ['webkit', 'webkitTransform','-webkit-transform'] in webkit browsers that support '-webkit-transform', but not 'transform'
+
+// Example #2:
+//  var transform = DeVendorCSS('transform');
+//  eval('e.style.'+transform[1]+'="translateY(5px)"')
+
+function DeVendorCSS(n) {
+    var v = ['', 'webkit', 'moz', 'o', 'ms'];
+    for (var i=0; i<v.length; i++)
+    if (((i)? '-'+v[i]+'-'+n : n) in document.body.style) return [v[i], v[i] + ((i)? n.charAt(0).toUpperCase()+n.slice(1) : n), ((i)? '-'+v[i]+'-' : '')+n];
+    return ['', n, n]; // Fallback for no support
+}
